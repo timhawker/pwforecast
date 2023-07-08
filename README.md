c568c19b391d3ba6115de882c70faa6eb5f7463a
Tim <timhawker@icloud.com>
2023-07-08 18:50:45 +1200

    Updated docstrings, methods public, updated readme

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 ## PwForecast
-A Python library to charge/discharge Powerwall based on solar forecast and peak/off peak tariffs. 
+
+A Python class to charge/discharge Powerwall based on solar forecast and peak/off peak tariffs. 
 
 Utilises the excellent [TeslaPy](https://tesla-api.timdorr.com/) by Tim Dorssers.
 
-Please Note: This is a work in progress. The first release tag will be applied when pwforecast is in a working state.
+PwForecast is primarily designed for those with dual rate tariffs. If there is interest in expanding this to provide
+more flexible control for those with more complex tariffs, please let me know. 
 
 
-## Usage
+## Getting Started
 
-Basic usage of the module:
+PwForecast requires a Solcast API key and Site ID. You can sign up for a 
+[free Hobbyist account](https://toolkit.solcast.com.au/register) which allows up to two sites and 50 calls per day. 
+
+[TeslaPy](https://tesla-api.timdorr.com/) has great documentation on getting started. It even works with two factor
+enabled.
+
+Rather than wrap [TeslaPy](https://tesla-api.timdorr.com/), PwForecast requires an instance of a Tesla class passed to 
+it. This allows you to configure the object based on your credentials before passing to PwForecast.  
+
+
+## Overview 
+
+PwForecast has two main methods, `set_peak_mode` and `set_off_peak_mode`. Both can be configured using a PwForecast
+instance. 
+
+Here is an example showing how to configure teslapy and PwForecast:
 
 ```python
 import teslapy
@@ -24,14 +41,37 @@ with teslapy.Tesla(email=tesla_email) as tesla:
     pw_forecast = PwForecast(teslapy_session=tesla,
                              solcast_api_key=solcast_api_key,
                              solcast_site_ids=solcast_site_ids)
-    
-    # Peak Mode: Sets powerwall backup reserve from the 'min_reserve_peak' setting.
-    pw_forecast.min_reserve_peak = 10  # set min peak reserve, default 20.
-    pw_forecast.set_peak_mode()
-    
-    # Off Peak Mode: Sets powerwall backup reserve based on solar forecast.
-    pw_forecast.min_reserve_off_peak = 25  # set min off peak reserve, default 30.
-    pw_forecast.max_reserve = 95  # set max allowed reserve, default 90.
-    pw_forecast.required_energy_peak = 20000  # set energy required during peak time in Wh, default 30000.
-    pw_forecast.set_off_peak_mode()
 ```
+
+
+
+## Setting Peak Mode
+
+Use this method when your peak rate starts. You can configure what backup reserve the Powerwall can discharge down
+to by setting the `min_reserve_peak_rate` value. 
+
+```python
+pw_forecast.min_reserve_peak_rate = 10  # Default 20
+pw_forecast.set_peak_mode()
+```
+
+
+## Setting Off-Peak Mode
+
+Use this method when your off-peak rate starts. This requires a little more configuration.
+ * Configure what backup reserve the Powerwall can discharge down to by setting the 'min_reserve_off_peak_rate' attribute.
+ * Configure the maximum backup reserve allowed by setting the 'max_reserve' attribute. 
+ * Configure the amount of energy you require during the peak-rate. 
+
+```python
+pw_forecast.min_reserve_off_peak_rate = 25  # Default 30
+pw_forecast.max_reserve = 95  # Default 100
+pw_forecast.required_energy_peak_rate = 20000  # Default 30000
+pw_forecast.set_off_peak_mode()
+```
+
+
+## Advanced Configuration
+
+PwForecast has a few advanced configuration options. Please check the class docstring for more info. 
+
--- __init__.py
+++ __init__.py
@@ -6,6 +6,10 @@ import parser
 import pprint
 
 
+# TODO: Publish python package on pypi.
+# TODO: Calculate
+
+
 class PwForecast(object):
     """
     A tool that dynamically sets Powerwall backup reserve percent based on solar forecast.
@@ -21,17 +25,46 @@ class PwForecast(object):
     Args:
         teslapy_session (teslapy.Tesla): The tesla session object.
         solcast_api_key (str): The Solcast API Key.
-        solcast_site_ids (dict): A dict of Solcast site ID's, where key is the site name and value is the site ID.
+        solcast_site_ids (dict): A dict of Solcast site ID's, where key is the site name and
+            value is the site ID.
 
     Attributes:
-        timezone (datetime.tzinfo): The tzdata timezone the system sits within. Will use timezone from system by
-            default.
-        min_reserve_peak (int): The minimum backup reserve to be set when in peak grid rates. Default 20.
-        min_reserve_off_peak (int): The minimum backup reserve that can be set when in off peak grid rates. Default 30.
-        max_reserve (int): The maximum backup reserve that can be set. Default 90.
-        required_energy_peak (int): The energy required during peak rate in Wh. Default 30000.
-
-        TODO: Advanced config attrs.
+        min_reserve_peak_rate (int): The minimum backup reserve to be set when in peak grid rates.
+            Default 20.
+        min_reserve_off_peak_rate (int): The minimum backup reserve that can be set when in off-peak
+            grid rates. Default 30.
+        max_reserve (int): The maximum backup reserve that can be set. If you don't like charging
+            your batteries to 100%, you can set this value. Default 100.
+        timezone (datetime.tzinfo): The tzdata timezone the system sits within. Will use timezone
+            from system by default.
+        required_energy_peak_rate (int): The amount of energy required during peak rate in Wh. When
+            setting peak mode, PwForecast will determine how much solar will be generated. It
+            will then calculate how much to fill the batteries based on the remaining energy
+            requirement. Default 30000.
+        global_retry (int): The number of times the set function should try before failing. This
+            is useful if there is a temporary service outage. Be careful setting this value too
+            high as solcast has a limited number of
+        reserve_retry (int): The number of retries to make when setting reserve percent. A failure
+            is defined as an unsuccessful switch transition by monitoring the power flow, even if
+            the tesla API returns success.
+        retry_sleep (int): The time in seconds to sleep before retrying if an error occurs.
+            Default 20.
+        wait_for_response_sleep (int): The time in seconds to sleep before testing the current
+            site power flow. This is useful as the Powerwall sometimes takes time to respond to
+            requests. Default 20.
+        reserve_switch_margin (int): The minimum number of watts to satisfy a successful backup
+            reserve power flow transition (e.g. from discharging to charging). When setting the
+            backup reserve higher than the current charge, the battery needs to be importing more
+            than this value. When setting the backup reserve lower than the current charge, the
+            sum of battery+solar needs to be producing more than this value. Default 100.
+        charge_margin (int): If the Powerwall current charge level is within this margin to the
+            value being set, the reserve_switch_margin is not monitored. This is necessary because
+            the Powerwall can have unexpected behaviour when setting percentages close to current
+            charge percentage. There have been cases where the Powerwall has started charging when
+            setting a backup reserve 0.7% less than current charge percentage, causing the reserve
+            switch checking logic to fail. Default 1.
+        full_pack_energy (int): The full pack energy in Wh of a brand new powerwall. This is used
+            to calculate the overall state of health of all batteries in the system. Default 14000.
 
     """
     def __init__(self, teslapy_session, solcast_api_key, solcast_site_ids):
@@ -42,13 +75,13 @@ class PwForecast(object):
         self._solcast_site_ids = solcast_site_ids
 
         # basic configuration
-        self.timezone = tzlocal.get_localzone()
-        self.min_reserve_peak = 20
-        self.min_reserve_off_peak = 30
-        self.max_reserve = 90
-        self.required_energy_peak = 30000
+        self.min_reserve_peak_rate = 20
+        self.min_reserve_off_peak_rate = 30
+        self.max_reserve = 100
+        self.required_energy_peak_rate = 30000
 
         # advanced configuration
+        self.timezone = tzlocal.get_localzone()
         self.global_retry = 5
         self.reserve_retry = 5
         self.retry_sleep = 30
@@ -57,70 +90,8 @@ class PwForecast(object):
         self.charge_margin = 1
         self.full_pack_energy = 14000
 
-    # public
-    def set_peak_mode(self):
-        """
-        A convenience method to set peak mode. Used when transitioning to peak rates.
-
-        Raises:
-            Exception: If an invalid charge state is detected and the retry limit has been
-                reached.
-
-        """
-        for i in range(1, self.global_retry + 1):
-            try:
-                # status update
-                msg = 'Setting peak mode, attempt {i} of {total}'
-                print(msg.format(i=i, total=self.global_retry))
-                # set the peak reserve
-                status = self._set_backup_reserve_percent(self.min_reserve_peak)
-                self._print_summary(**status)
-            except Exception as e:
-                if i == self.global_retry:
-                    raise
-                msg = '{c}: {e}'
-                print(msg.format(c=e.__class__.__name__, e=e))
-                # sleep in case of temporary outage
-                time.sleep(self.retry_sleep)
-            else:
-                break
-
-    def set_off_peak_mode(self):
-        """
-        A convenience method to set off-peak mode. Used when transitioning to off-peak
-        rates.
-
-        The powerwall backup reserve will be set based on solar forecast tomorrow and configured settings.
-
-        Raises:
-            Exception: If an invalid charge state is detected and the retry limit has been
-                reached.
-
-        """
-        for i in range(1, self.global_retry + 1):
-            try:
-                # status update
-                msg = 'Setting off peak mode, attempt {i} of {total}'
-                print(msg.format(i=i, total=self.global_retry))
-                # get the off peak reserve
-                forecast_tomorrow = self._get_solar_forecast_tomorrow()
-                charge_percent = self._get_backup_reserve_percent(forecast_tomorrow)
-                # set the off peak reserve
-                status = self._set_backup_reserve_percent(charge_percent)
-                status['solar_forecast'] = forecast_tomorrow
-                self._print_summary(**status)
-            except Exception as e:
-                if i == self.global_retry:
-                    raise
-                msg = '{c}: {e}'
-                print(msg.format(c=e.__class__.__name__, e=e))
-                # sleep in case of temporary outage
-                time.sleep(self.retry_sleep)
-            else:
-                break
-
     # solcast
-    def _get_solar_forecast_tomorrow(self):
+    def get_solar_forecast_tomorrow(self):
         """
         Gets estimated solar production in Wh for tomorrow via Solcast API.
 
@@ -164,7 +135,7 @@ class PwForecast(object):
         return int(total_energy_kwh * 1000)
 
     # powerwall
-    def _get_backup_reserve_percent(self, forecast_production):
+    def calculate_backup_reserve(self, forecast_production):
         """
         Calculates a suitable backup reserve percent for the powerwall based on provided
         forecast production.
@@ -182,9 +153,9 @@ class PwForecast(object):
         total_pack_energy = battery_data['total_pack_energy']
 
         available_energy = forecast_production
-        charge_percent = self.min_reserve_peak
+        charge_percent = self.min_reserve_peak_rate
         pack_energy_increment = total_pack_energy / 100
-        while available_energy < self.required_energy_peak:
+        while available_energy < self.required_energy_peak_rate:
             available_energy += pack_energy_increment
             charge_percent += 1
             if charge_percent >= self.max_reserve:
@@ -192,7 +163,7 @@ class PwForecast(object):
 
         return int(charge_percent)
 
-    def _set_backup_reserve_percent(self, percent_target):
+    def set_backup_reserve_percent(self, percent_target):
         """
         Sets the provided backup reserve percent. If an invalid state is detected after
         setting the reserve percent, the function will retry up to a max number of times
@@ -285,6 +256,68 @@ class PwForecast(object):
                 'reserve': percent_target,
                 'capacity': total_pack_energy,
                 'soh': pack_soh}
+
+    # public
+    def set_peak_mode(self):
+        """
+        Sets backup reserve to 'min_reserve_peak_rate'. Used when transitioning to peak rates.
+
+        Attempts to set mode multiple times if an error occurs up to the 'global_retry' limit.
+
+        Raises:
+            Exception: If an invalid charge state is detected and the retry limit has been
+                reached.
+
+        """
+        for i in range(1, self.global_retry + 1):
+            try:
+                # status update
+                msg = 'Setting peak mode, attempt {i} of {total}'
+                print(msg.format(i=i, total=self.global_retry))
+                # set the peak reserve
+                status = self._set_backup_reserve_percent(self.min_reserve_peak_rate)
+                self._print_summary(**status)
+            except Exception as e:
+                if i == self.global_retry:
+                    raise
+                msg = '{c}: {e}'
+                print(msg.format(c=e.__class__.__name__, e=e))
+                # sleep in case of temporary outage
+                time.sleep(self.retry_sleep)
+            else:
+                break
+
+    def set_off_peak_mode(self):
+        """
+        Sets backup reserve based on solar forecast tomorrow. The backup reserve will be set between
+        'min_reserve_off_peak_rate' and 'max_reserve'. Used when transitioning to off-peak rates.
+
+        Raises:
+            Exception: If an invalid charge state is detected and the retry limit has been
+                reached.
+
+        """
+        for i in range(1, self.global_retry + 1):
+            try:
+                # status update
+                msg = 'Setting off-peak mode, attempt {i} of {total}'
+                print(msg.format(i=i, total=self.global_retry))
+                # get the off-peak reserve
+                forecast_tomorrow = self.get_solar_forecast_tomorrow()
+                charge_percent = self.calculate_backup_reserve(forecast_tomorrow)
+                # set the off-peak reserve
+                status = self.set_backup_reserve_percent(charge_percent)
+                status['solar_forecast'] = forecast_tomorrow
+                self._print_summary(**status)
+            except Exception as e:
+                if i == self.global_retry:
+                    raise
+                msg = '{c}: {e}'
+                print(msg.format(c=e.__class__.__name__, e=e))
+                # sleep in case of temporary outage
+                time.sleep(self.retry_sleep)
+            else:
+                break
 
     # internal
     def _print_summary(self, soc, reserve, capacity, soh, solar_forecast=None):
