e3b605dbf87f32002940af76fd937b46ad578198
Tim <timhawker@icloud.com>
2023-07-08 19:28:20 +1200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setuptools.setup(
     name='pwforecast',
     author='Tim Hawker',
     description='A Python module to charge/discharge Powerwall based on solar forecast and peak/off peak tariffs.',
-    url='https://github.com/timhawker/pwforecast'
+    url='https://github.com/timhawker/pwforecast',
+    long_description_content_type='text/markdown',
+    long_description='file: README.md'
 )
