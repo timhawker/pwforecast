4c548de940adc4f8749b2b401a1bbb6280ce0dd7
Tim <timhawker@icloud.com>
2023-07-08 19:33:36 +1200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 import setuptools
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setuptools.setup(
     name='pwforecast',
+    version='0.1.4',
     author='Tim Hawker',
     description='A Python module to charge/discharge Powerwall based on solar forecast and peak/off peak tariffs.',
     url='https://github.com/timhawker/pwforecast',
     long_description_content_type='text/markdown',
-    long_description='file: README.md'
+    long_description=long_description
 )
