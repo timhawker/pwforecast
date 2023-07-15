fd07a6a01bf11fdf27ec2fce84d460999aae460e
Tim <timhawker@icloud.com>
2023-07-16 09:45:22 +1200

    Updated for next release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = (this_directory / 'README.md').read_text()
 
 setuptools.setup(
     name='pwforecast',
-    version='1.1.2',
+    version='1.1.3',
     author='Tim Hawker',
     license='MIT',
     url='https://github.com/timhawker/pwforecast',
